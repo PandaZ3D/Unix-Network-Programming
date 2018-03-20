ca0b83bdd6a934310f5f4f688e6c5ac120818b52
Allen Aboytes <aaboytes@ucsc.edu>
2018-03-20 01:38:04 -0700

    added basic filler functions and s/c C files

--- final/src/ftp.c
+++ final/src/ftp.c
@@ -8,3 +8,44 @@ struct packet_t
 		uint8_t arglen;				/* 1 byte 		: [length of args] */
 		char arg[MAXARG];		/* 255 bytes : [cmd message] */
 } __attribute__ ((packed));
+
+/* creates a listening socket to be used for incomming connections */
+int listensocket(int socketfd)
+{
+	return 0;
+}
+
+/* sends cmd defined in packet */
+void sendcmd(int socketfd, pkt cmd)
+{
+	
+}
+/* returns packet buffered in socket */
+pkt recvcmd(int)
+{
+	return NULL;
+}
+
+/* sends a chunk of data, returns amount of bytes sent */
+int sendchunk(int socketfd, char* chunk)
+{
+	return 0;
+}
+
+/* recieves a chunk of data, returns pointer to chunk */
+char* recvchunk(int socketfd)
+{
+	return NULL;
+}
+
+/* sends a whole file, returns bytes sent */
+int sendfile(int socketfd, int putfd)
+{
+		return 0;
+}
+
+/* recieves a whole file, returns bytes recvd */
+int recvfile(int socketfd)
+{
+		return 0;
+}
