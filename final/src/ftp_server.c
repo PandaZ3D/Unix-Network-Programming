8b8d0d9f3b157416aea63cb0d9792cb5cabc42c7
Allen Aboytes <aaboytes@ucsc.edu>
2018-03-20 03:13:47 -0700

    added connect and accept calls in s/c main

--- final/src/ftp_server.c
+++ final/src/ftp_server.c
@@ -1,7 +1,43 @@
+/*
+ * Allen Aboytes
+ * CMPE156 - final
+ * Parsa - Winter 18
+ * 
+ * ftp_server.c
+ * client side of ftp server application
+ */
+ 
 #include "netlib.h"
 #include "ftp.h"
 
 int main(int argc, char** argv)
 {
+	/* check command line arguments */
+	if(argc != 2)
+	{
+		fprintf(stderr, "%s <listen-port>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	
+	/* get the server port number of server */
+	int portno = atoi(argv[PORT]);
+	/* set up server IP information */
+	struct sockaddr_in server_addr;
+	bzero((char*) &server_addr, sizeof server_addr);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(portno);
+	server_addr.sin_family = inet_addr(INADDR_ANY);
+	socklen_t serverlen = sizeof server_addr;
+	
+	/* client opens up both command channel socket and data socket */
+	int cmdsockfd = listensocket(portno);
+	
+	/* we now wait for a new client connection */
+	struct sockaddr_in client_addr;
+	socklen_t clientlen = sizeof client_addr;
+	
+	int newclientfd = accept(cmdsockfd, (struct sockaddr*) );
+	
+	close(cmdsockfd);
 	return EXIT_SUCCESS;
 }
